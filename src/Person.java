a14bdeac516314d91c99b67125e35097ca771f9a
sz szabolcs <szitha.szabolcs@gmail.com>
2020-05-11 14:34:29 +0200

    Fixes to Person class

--- src/Person.java
+++ src/Person.java
@@ -1,25 +1,47 @@
 public class Person {
-    private String name;
-    private int age;
+    private String firstName;
+    private String lastName;
+    private String city;
+    private String email;
 
-    public Person(String name, int age) {
-        this.name = name;
-        this.age = age;
+    public Person(String firstName, String lastName, String city, String email) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.city = city;
+        this.email = email;
     }
 
-    public String getName() {
-        return name;
+    public String getFirstName() {
+        return firstName;
     }
 
-    public void setName(String name) {
-        this.name = name;
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
     }
 
-    public int getAge() {
-        return age;
+    public String getLastName() {
+        return lastName;
     }
 
-    public void setAge(int age) {
-        this.age = age;
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
     }
+
+    public String getCity() {
+        return city;
+    }
+
+    public void setCity(String city) {
+        this.city = city;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+
 }
